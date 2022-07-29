7386b8bd96ad63519176d73fd0efbabb40c3b85b
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-07-29 17:12:24 +0800

    [SQUASHED] base: Add status bar lyric for Android 14
    * This is a squash of:
    
    base: Add status bar lyric [1/3]
    
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: I5c49187b0752286ee00a60d961203c0c4ec0d555
    
    fixup! base: Add status bar lyric [1/3]
    
     * java.lang.SecurityException: Settings key: <status_bar_show_lyric> is
       not readable. From S+, settings keys annotated with @hide are
       restricted to system_server and system apps only, unless they are
       annotated with @Readable.
    
    Change-Id: Ia0c209f3ddd5bbe1be676e172f408b3faf8cfe81
    
    base: Adapt StatusBar Lyric for Miku UI Udon
    
    Signed-off-by: AmeChanRain <downloadbot007@gmail.com>
    
    Co-authored-by: LynnrinChan <wuxiaofei31@gmail.com>
    Co-authored-by: AmeChanRain <downloadbot007@gmail.com>
    Change-Id: I55d8299268a633de9b8b9fcb1d9ebab7dbe901fc
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -15857,6 +15857,18 @@ public final class Settings {
          * @hide
          */
         public static final String SPOOF_TRICKYSTORE_PATCH = "spoof_trickystore_patch";
+        
+        /**
+         * Show lyric in status bar when playing music
+         * <ul>
+         *    <li> 0 = disabled </li>
+         *    <li> 1 = enabled </li>
+         * </ul>
+         *
+         * @hide
+         */
+        @Readable
+        public static final String STATUS_BAR_SHOW_LYRIC = "status_bar_show_lyric";
     }
 
     /**
