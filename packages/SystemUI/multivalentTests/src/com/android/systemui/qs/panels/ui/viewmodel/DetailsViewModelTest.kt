c6a658932d0ad031dd5a417875a39c31d4e49c1a
Jiaming Cheng <jiamingc@google.com>
2025-12-03 02:15:17 +0000

    [QSDetailedView] Add user metrics for details view
    Bug: 365584137
    Flag: com.android.systemui.qs_tile_detailed_view
    Test: DetailsViewModelTest
    Change-Id: I2bb47b2f870dbe4452bbc8279095856488f870bb
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/multivalentTests/src/com/android/systemui/qs/panels/ui/viewmodel/DetailsViewModelTest.kt
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/qs/panels/ui/viewmodel/DetailsViewModelTest.kt
@@ -18,10 +18,13 @@ package com.android.systemui.qs.panels.ui.viewmodel
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import com.android.internal.logging.uiEventLoggerFake
+import com.android.systemui.Flags.FLAG_DUAL_SHADE
 import com.android.systemui.SysuiTestCase
 import com.android.systemui.flags.EnableSceneContainer
 import com.android.systemui.kosmos.testScope
 import com.android.systemui.qs.FakeQSTile
+import com.android.systemui.qs.QSEvent
 import com.android.systemui.qs.pipeline.data.repository.tileSpecRepository
 import com.android.systemui.qs.pipeline.domain.interactor.currentTilesInteractor
 import com.android.systemui.qs.pipeline.shared.TileSpec
@@ -129,5 +132,82 @@ class DetailsViewModelTest : SysuiTestCase() {
                 assertThat(underTest.onTileClicked(specNoDetails)).isFalse()
                 assertThat(underTest.activeTileDetails).isNull()
             }
+        }
+
+    @Test
+    @EnableFlags(FLAG_DUAL_SHADE)
+    fun testMetricsLogging_openDetailsView() =
+        kosmos.runTest {
+            enableDualShade()
+            val specs = listOf(spec)
+            tileSpecRepository.setTiles(userId = 0, specs)
+            val tiles by collectLastValue(currentTilesInteractor.currentTiles)
+            val firstTile = checkNotNull(tiles)[0]
+
+            // Open first tile details
+            underTest.onTileClicked(spec)
+
+            assertThat(uiEventLoggerFake.numLogs()).isEqualTo(1)
+            val event = uiEventLoggerFake.get(0)
+            assertThat(event.eventId).isEqualTo(QSEvent.QS_DETAILS_OPEN.id)
+            assertThat(event.packageName).isEqualTo(firstTile.tile.metricsSpec)
+        }
+
+    @Test
+    @EnableFlags(FLAG_DUAL_SHADE)
+    fun testMetricsLogging_settingsClick() =
+        kosmos.runTest {
+            enableDualShade()
+            val specs = listOf(spec)
+            tileSpecRepository.setTiles(userId = 0, specs)
+            val tiles by collectLastValue(currentTilesInteractor.currentTiles)
+            val firstTile = checkNotNull(tiles)[0]
+
+            // Open first tile details to set _activeTile
+            underTest.onTileClicked(spec)
+
+            assertThat(uiEventLoggerFake.numLogs()).isEqualTo(1)
+            val event1 = uiEventLoggerFake.get(0)
+            assertThat(event1.eventId).isEqualTo(QSEvent.QS_DETAILS_OPEN.id)
+            assertThat(event1.packageName).isEqualTo(firstTile.tile.metricsSpec)
+
+            // Click settings
+            underTest.logOnSettingsClicked()
+
+            assertThat(uiEventLoggerFake.numLogs()).isEqualTo(2)
+            val event2 = uiEventLoggerFake.get(1)
+            assertThat(event2.eventId).isEqualTo(QSEvent.QS_DETAILS_SETTINGS_CLICK.id)
+            assertThat(event2.packageName).isEqualTo(firstTile.tile.metricsSpec)
+        }
+
+    @Test
+    @EnableFlags(FLAG_DUAL_SHADE)
+    fun testMetricsLogging_closeDetailsView() =
+        kosmos.runTest {
+            enableDualShade()
+            val spec2 = TileSpec.create("another_tile")
+            val specs = listOf(spec, spec2)
+            tileSpecRepository.setTiles(userId = 0, specs)
+            val tiles by collectLastValue(currentTilesInteractor.currentTiles)
+            val firstTile = checkNotNull(tiles)[0]
+            val secondTile = checkNotNull(tiles)[1]
+
+            // Open first tile details
+            underTest.onTileClicked(spec)
+            // Open second tile details, which should close the first one
+            underTest.onTileClicked(spec2)
+
+            assertThat(uiEventLoggerFake.numLogs()).isEqualTo(3) // 2 opens, 1 close
+            val closeEvent = uiEventLoggerFake.get(1) // open (0) -> close (1) -> open (2)
+            assertThat(closeEvent.eventId).isEqualTo(QSEvent.QS_DETAILS_CLOSE.id)
+            assertThat(closeEvent.packageName).isEqualTo(firstTile.tile.metricsSpec)
+
+            // Close the currently active details view (second tile)
+            underTest.closeDetailedView()
+
+            assertThat(uiEventLoggerFake.numLogs()).isEqualTo(4) // +1 close
+            val event = uiEventLoggerFake.get(3)
+            assertThat(event.eventId).isEqualTo(QSEvent.QS_DETAILS_CLOSE.id)
+            assertThat(event.packageName).isEqualTo(secondTile.tile.metricsSpec)
         }
 }
