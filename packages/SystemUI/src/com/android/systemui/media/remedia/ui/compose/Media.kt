4aba07d01530129e2de7e437d4874994b3ce4410
Michael Mikhail <michaelmikhil@google.com>
2026-02-23 18:16:12 +0000

    Use graphic layer to clip media
    Flag: com.android.systemui.media_controls_in_compose
    Flag: com.android.systemui.scene_container
    Bug: 471142000
    Bug: 486373722
    Test: Checked UI - swipe in QS
    
    Change-Id: Ibf32ca46bba66c1dd1e616db30be077607e1bdaf
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/media/remedia/ui/compose/Media.kt
+++ packages/SystemUI/src/com/android/systemui/media/remedia/ui/compose/Media.kt
@@ -91,7 +91,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.Offset
@@ -254,14 +253,19 @@ private fun CardCarouselContent(
 
     Box(
         modifier =
-            modifier.clip(roundedCornerShape).clipToBounds().pointerInput(behavior) {
-                if (behavior.isCarouselScrollFalseTouch != null) {
-                    awaitEachGesture {
-                        awaitFirstDown(false, PointerEventPass.Initial)
-                        isFalseTouchDetected = behavior.isCarouselScrollFalseTouch.invoke()
+            modifier
+                .graphicsLayer {
+                    shape = roundedCornerShape
+                    clip = true
+                }
+                .pointerInput(behavior) {
+                    if (behavior.isCarouselScrollFalseTouch != null) {
+                        awaitEachGesture {
+                            awaitFirstDown(false, PointerEventPass.Initial)
+                            isFalseTouchDetected = behavior.isCarouselScrollFalseTouch.invoke()
+                        }
                     }
                 }
-            }
     ) {
         @Composable
         fun PagerContent(overscrollEffect: OverscrollEffect? = null) {
