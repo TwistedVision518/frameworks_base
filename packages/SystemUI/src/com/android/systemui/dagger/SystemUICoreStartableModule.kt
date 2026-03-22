599a562fae986b90aeebfaeba20e9f198838a3cd
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-03-22 15:47:06 +0800

    SystemUI: Adding usb prompt bottomsheet
    Change-Id: I0c4a5eec93fbac5a8b0eb55aa6adf83840a27066
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
@@ -63,6 +63,7 @@ import com.android.systemui.statusbar.notification.headsup.StatusBarHeadsUpChang
 import com.android.systemui.stylus.StylusUsiPowerStartable
 import com.android.systemui.temporarydisplay.chipbar.ChipbarCoordinator
 import com.android.systemui.usb.StorageNotification
+import com.android.systemui.usb.UsbModePickerDialogDelegate
 import com.android.systemui.util.NotificationChannels
 import com.android.systemui.wmshell.WMShell
 import dagger.Binds
@@ -190,6 +191,12 @@ abstract class SystemUICoreStartableModule {
     @IntoMap
     @ClassKey(StorageNotification::class)
     abstract fun bindStorageNotification(sysui: StorageNotification): CoreStartable
+
+    /** Inject into UsbModePickerDialogDelegate. */
+    @Binds
+    @IntoMap
+    @ClassKey(UsbModePickerDialogDelegate::class)
+    abstract fun bindUsbModePickerDialogDelegate(impl: UsbModePickerDialogDelegate): CoreStartable
 
     /** Inject into MediaOutputSwitcherDialogUI. */
     @Binds
