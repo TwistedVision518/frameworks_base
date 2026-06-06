a63377b1edbfc7f2188ac1926174cd6a41d33734
Ghosuto <clash.raja10@gmail.com>
2026-06-06 15:47:48 +0000

    SystemUI: Add clock style wobble charging animation [1/2]
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14895,6 +14895,12 @@ public final class Settings {
         public static final String LOCK_SCREEN_CUSTOM_CLOCK_ALBUM_ART_COLOR = "lock_screen_custom_clock_album_art_color";
 
         /**
+         * Custom clock wobble
+         * @hide
+         */
+        public static final String LOCK_SCREEN_CUSTOM_CLOCK_WOBBLE_ON_CHARGE = "lock_screen_custom_clock_wobble_on_charge";
+
+        /**
          * Timeout length for clipboard auto clear
          * @hide
          */
